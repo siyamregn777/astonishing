03592d2c5708ef5c233fb7d8321488745464eea1
amare-tesema <siyamregnyeshidagna1234@gmail.com>
2025-08-19 22:29:10 +0300

    chore: update dependencies and configuration files
    - Added new dependencies for Radix UI, React Three, Tabler Icons, TSParticles, and Tailwind CSS utilities.
    - Removed the old PostCSS configuration file in favor of a new one that includes Tailwind CSS and Autoprefixer.
    - Created a new Tailwind CSS configuration file with custom theme settings and animations.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,31 @@
-
+import Navbar from "@/components/Navbar";
+import {AuroraBackgroundDemo} from "@/components/AuroraBackgroundDemo"
+import {BackgroundBeamsWithCollisionDemo} from "@/components/BackgroundBeamsWithCollisionDemo.jsx"
+import {BackgroundBoxesDemo} from "@/components/BackgroundBoxesDemo.jsx";
+import {CardHoverEffectDemo} from "@/components/CardHoverEffectDemo.jsx";
+import {CompareDemo} from "@/components/CompareDemo.jsx"
+import {FloatingDockDemo} from "@/components/FloatingDockDemo.jsx";
+import {GlobeDemo} from "@/components/GlobeDemo.jsx"
+import {StickyScrollRevealDemo} from "@/components/StickyScrollRevealDemo.jsx";
+import {TextGenerateEffectDemo} from "@/components/TextGenerateEffectDemo.jsx"
+import { TimelineDemo } from "@/components/TimelineDemo";
+import { WavyBackgroundDemo } from "@/components/WavyBackgroundDemo.jsx";
 export default function Home() {
   return (
-    <div >
-      <h1 className="text-4xl font-bold">wow</h1>
-    </div>
+   <div>
+    <Navbar />
+    <WavyBackgroundDemo/>
+    <AuroraBackgroundDemo />
+    <BackgroundBeamsWithCollisionDemo />
+    <BackgroundBoxesDemo />
+    <CardHoverEffectDemo />
+    <CompareDemo />
+    <TextGenerateEffectDemo/>
+    <GlobeDemo />
+    <StickyScrollRevealDemo/>
+    <TimelineDemo/>
+    <FloatingDockDemo />
+   </div>
 
   );
 }
